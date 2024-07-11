608f4ac2d6ec17b41ef4a54bca1a4c24f84ab6df
manju-sri <107233576+manju-sri@users.noreply.github.com>
2024-07-11 16:07:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+
+function submitForm() {
+
+	var firstName = document.getElementById(firstName).value
+	var lastName = document.getElementById(lastName).value;
+	var phNo = document.getElementById(phNo).value;
+	var emailId = document.getElementById(emailId).value;
+
+	var alertMsg = "First Name: " + firstName + "\n" +
+		            "Last Name: " + lastName + "\n" +
+		            "Phone Number: " + phNo + "\n" +
+		            "Email ID: " + emailId + "\n";
+
+	alert(alertMsg);
+	
+	
