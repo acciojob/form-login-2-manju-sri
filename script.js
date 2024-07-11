676d5917ad3e36fa7057a410bac9ffd3186dacef
manju-sri <107233576+manju-sri@users.noreply.github.com>
2024-07-11 16:27:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 
 function submitForm() {
 
-	var firstName = document.getElementById(firstName).value
-	var lastName = document.getElementById(lastName).value;
-	var phNo = document.getElementById(phNo).value;
-	var emailId = document.getElementById(emailId).value;
+	var firstName = document.getElementById("firstName").value
+	var lastName = document.getElementById("lastName").value;
+	var phNo = document.getElementById("phNo").value;
+	var emailId = document.getElementById("emailId").value;
 
 	var alertMsg = "First Name: " + firstName + "\n" +
 		            "Last Name: " + lastName + "\n" +
